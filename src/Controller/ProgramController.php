04de110503a261bd23a600be750ebed8b8b6e863
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-06-01 21:47:23 +0200

    symfony 11

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
 use App\Entity\Program;
 use App\Entity\Category;
 use App\Entity\Season;
@@ -49,10 +51,10 @@ class ProgramController extends AbstractController
      * @return Response
      */
 
-    public function show(int $id): Response
+    public function show(Program $program): Response
 
     {
-        $program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $id]);
+        /*$program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $id]);*/
         $seasons = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['program' => $program]);
         if (!$program) {
             throw $this->createNotFoundException(
@@ -72,27 +74,29 @@ class ProgramController extends AbstractController
      * Getting a season by id
      * 
      * @Route("/{programId}/season/{seasonId}", name="season_show")
+     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"programId": "id"}})
+     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"seasonId": "id"}})
      * @return Response
      */
 
-     public function showSeason( int $programId, int $seasonId): Response
+     public function showSeason(Program $program, Season $season): Response
 
      {
-        $program = $this->getDoctrine()->getRepository(Program::class)->find($programId);
+        /*$program = $this->getDoctrine()->getRepository(Program::class)->find($programId);*/
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with id : '.$id. ' found in program\'s table.'
             );
         }
 
-        $season = $this->getDoctrine()->getRepository(Season::class)->find($seasonId);
+        /*$season = $this->getDoctrine()->getRepository(Season::class)->find($seasonId);*/
         if (!$season) {
             throw $this->createNotFoundException(
                 'No season with id : '.$id. ' found in season\'s table.'
             );
         }
 
-        $episodes = $this->getDoctrine()->getRepository(Episode::class)->findBy(['season' => $seasonId ]);
+        $episodes = $this->getDoctrine()->getRepository(Episode::class)->findBy(['season' => $season->getId() ]);
 
         return $this->render('program/season_show.html.twig', [
     
@@ -101,5 +105,41 @@ class ProgramController extends AbstractController
             'episodes' => $episodes,
          ]);
      }
+
+         /**
+     * @Route("/{programId}/seasons/{seasonId}/episodes/{episodeId}", name="episode_show")
+     * @ParamConverter("program", class="App\Entity\Program", options={"mapping": {"programId": "id"}})
+     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"seasonId": "id"}})
+     * @ParamConverter("episode", class="App\Entity\Episode", options={"mapping": {"episodeId": "id"}})
+     * @return Response
+     */
+
+    public function showEpisode(Program $program, Season $season, Episode $episode): Response
+        
+    {
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $program->getId() . ' found in program\'s table.'
+            );
+        }
+
+        if (!$season) {
+            throw $this->createNotFoundException(
+                'No season with id : ' . $season->getId() . ' found in season\'s table.'
+            );
+        }
+
+        if (!$episode) {
+            throw $this->createNotFoundException(
+                'No episode with id : ' . $episode->getId() . ' found in episode\'s table.'
+            );
+        }
+
+        return $this->render('program/episode_show.html.twig', [
+            'program' => $program,
+            'season' => $season,
+            'episode' => $episode,
+        ]);
+    }
 
 }
