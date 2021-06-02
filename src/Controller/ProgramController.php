5f6f17fa925edce4e888ae4c0ef85c227c957fee
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-06-02 21:55:21 +0200

    Symfony 12

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -17,7 +17,8 @@ use App\Entity\Program;
 use App\Entity\Category;
 use App\Entity\Season;
 use App\Entity\Episode;
-
+use App\Form\ProgramType;
+use Symfony\Component\HttpFoundation\Request;
     /**
 
     * @Route("/programs", name="program_")
@@ -44,6 +45,65 @@ class ProgramController extends AbstractController
 
         ]);
     }
+
+    /**
+
+     * The controller for the program add form
+
+     * Display the form or deal with it
+
+     *
+
+     * @Route("/new", name="new")
+
+     */
+
+    public function new(Request $request) : Response
+
+    {
+
+        // Create a new Program Object
+
+        $program = new Program();
+
+        // Create the associated Form
+
+        $form = $this->createForm(ProgramType::class, $program);
+
+        // Get data from HTTP request
+
+        $form->handleRequest($request);
+
+        // Was the form submitted ?
+
+        if ($form->isSubmitted()) {
+
+            // Deal with the submitted data
+    
+            // Get the Entity Manager
+    
+            $entityManager = $this->getDoctrine()->getManager();
+    
+            // Persist Program Object
+    
+            $entityManager->persist($program);
+    
+            // Flush the persisted object
+    
+            $entityManager->flush();
+    
+            // Finally redirect to categories list
+    
+            return $this->redirectToRoute('program_index');
+    
+        }
+
+        // Render the form
+
+        return $this->render('program/new.html.twig', ["form" => $form->createView()]);
+
+    }
+
     /**
      * Getting a program by id
      * 
@@ -55,7 +115,7 @@ class ProgramController extends AbstractController
 
     {
         /*$program = $this->getDoctrine()->getRepository(Program::class)->findOneBy(['id' => $id]);*/
-        $seasons = $this->getDoctrine()->getRepository(Season::class)->findOneBy(['program' => $program]);
+        $seasons = $this->getDoctrine()->getRepository(Season::class)->findBy(['program' => $program]);
         if (!$program) {
             throw $this->createNotFoundException(
                 'No program with id : '.$id. ' found in program\'s table.'
