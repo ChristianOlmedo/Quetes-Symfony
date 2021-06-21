b37cbe93a78f987085ae011320a7a211ee12785d
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-06-21 11:41:04 +0200

    Quete 27

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+
 
 Class DefaultController extends AbstractController
 
@@ -32,6 +35,18 @@ Class DefaultController extends AbstractController
     
         ]);
     
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+
+    {
+
+        return $this->render('layout/navbartop.html.twig', [
+
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
+
+    ]);
+
     }
 
 }
